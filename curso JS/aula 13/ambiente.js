cf086facfde0a3921a533f179febd54d2757ccf2
Letícia Andrade <102621780+leticiaandrades@users.noreply.github.com>
2022-05-23 16:25:49 -0300

    aulas atualizadas

---
+++ curso JS/aula 13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/*
+var c = 1
+while (c <= 16) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
